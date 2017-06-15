39d7a02e83a222a896aef938b9b1c89a39314c55
qianlongwuyong900 <2246149620@qq.com>
2017-06-15 08:59:57 +0800

    添加联想图纸

---
+++ 联想/T410I点位图/新建文件夹/T410[1].php
@@ -0,0 +1,346 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gbk" />
+<title>提示信息_91修论坛</title>
+<script type='text/javascript' src='http://cbjs.baidu.com/js/s.js'></script><meta name="keywords" content="" />
+<meta name="description" content=",91修论坛" />
+<meta name="generator" content="Discuz! X2.5" />
+<meta name="author" content="Discuz! Team and Comsenz UI Team" />
+<meta name="copyright" content="2001-2012 Comsenz Inc." />
+<meta name="MSSmartTagsPreventParsing" content="True" />
+<meta http-equiv="MSThemeCompatible" content="Yes" />
+<base href="http://www.91xiubbs.com/" /><link rel="stylesheet" type="text/css" href="data/cache/style_1_common.css?08b" /><link rel="stylesheet" type="text/css" href="data/cache/style_1_forum_attachment.css?08b" /><script type="text/javascript">var STYLEID = '1', STATICURL = 'static/', IMGDIR = 'static/image/common', VERHASH = '08b', charset = 'gbk', discuz_uid = '0', cookiepre = 'STnV_2132_', cookiedomain = '', cookiepath = '/', showusercard = '0', attackevasive = '0', disallowfloat = 'login|newthread', creditnotice = '1|威望|,2|金钱|金币,3|推广积分|,4|上传|,5|下载|,6|钻石|,8|短信|', defaultstyle = '', REPORTURL = 'aHR0cDovL3d3dy45MXhpdWJicy5jb20vZm9ydW0ucGhwP21vZD1hdHRhY2htZW50JmFpZD1NVEEwTlRjMWZHVm1OVFkzTm1KaWZERXpORE00TnpVMk16VjhORFl3TnpJMWZERTVPREl5TXclM0QlM0QmY2s9Y2I4YTExODA=', SITEURL = 'http://www.91xiubbs.com/', JSPATH = 'data/cache/';</script>
+<script src="data/cache/common.js?08b" type="text/javascript"></script>
+<meta name="application-name" content="91修论坛" />
+<meta name="msapplication-tooltip" content="91修论坛" />
+<meta name="msapplication-task" content="name=门户;action-uri=http://www.91xiubbs.com/portal.php;icon-uri=http://www.91xiubbs.com/static/image/common/portal.ico" /><meta name="msapplication-task" content="name=论坛;action-uri=http://www.91xiubbs.com/forum.php;icon-uri=http://www.91xiubbs.com/static/image/common/bbs.ico" />
+<meta name="msapplication-task" content="name=群组;action-uri=http://www.91xiubbs.com/group.php;icon-uri=http://www.91xiubbs.com/static/image/common/group.ico" /><meta name="msapplication-task" content="name=家园;action-uri=http://www.91xiubbs.com/home.php;icon-uri=http://www.91xiubbs.com/static/image/common/home.ico" /><script src="data/cache/forum.js?08b" type="text/javascript"></script>
+</head>
+
+<body id="nv_forum" class="pg_attachment" onkeydown="if(event.keyCode==27) return false;">
+<div id="append_parent"></div><div id="ajaxwaitid"></div>
+
+<div id="toptb" class="cl">
+<div class="wp">
+<div class="z"><a href="javascript:;"  onclick="setHomepage('http://www.91xiubbs.com/');">设为首页</a><a href="http://www.91xiubbs.com/"  onclick="addFavorite(this.href, '91修论坛');return false;">收藏本站</a><a href="http://cang.baidu.com/do/add?it=91修网&iu=http://www.91xiubbs.com" >百度搜藏</a><script type="text/javascript">var _speedMark = new Date();</script></div>
+<div class="y">
+<a href="plugin.php?id=auction" title="抽奖，竞拍，兑换"  style="color: red">积分商城</a><a href="javascript:;" onclick="widthauto(this)">切换到宽版</a>
+</div>
+</div>
+</div>
+
+
+<div id="qmenu_menu" class="p_pop blk" style="display: none;">
+<div class="ptm pbw hm">
+请 <a href="javascript:;" class="xi2" onclick="lsSubmit()"><strong>登录</strong></a> 后使用快捷导航<br />没有帐号？<a href="member.php?mod=register" class="xi2 xw1">注册</a>
+</div>
+</div>
+<div id="hd">
+<div class="wp">
+<div class="hdc cl"><h2><a href="./" title="91修论坛"><img src="static/image/common/logo.png" alt="91修论坛" border="0" /></a></h2>
+
+<script src="data/cache/logging.js?08b" type="text/javascript"></script>
+<form method="post" autocomplete="off" id="lsform" action="member.php?mod=logging&amp;action=login&amp;loginsubmit=yes&amp;infloat=yes&amp;lssubmit=yes" onsubmit="return lsSubmit();">
+<div class="fastlg cl">
+<span id="return_ls" style="display:none"></span>
+<div class="y pns">
+<table cellspacing="0" cellpadding="0">
+<tr>
+<td><label for="ls_username">帐号</label></td>
+<td><input type="text" name="username" id="ls_username" class="px vm xg1"  value="UID/用户名/Email" onfocus="if(this.value == 'UID/用户名/Email'){this.value = '';this.className = 'px vm';}" onblur="if(this.value == ''){this.value = 'UID/用户名/Email';this.className = 'px vm xg1';}" tabindex="901" /></td>
+<td class="fastlg_l"><label for="ls_cookietime"><input type="checkbox" name="cookietime" id="ls_cookietime" class="pc" value="2592000" tabindex="903" />自动登录</label></td>
+<td>&nbsp;<a href="javascript:;" onclick="showWindow('login', 'member.php?mod=logging&action=login&viewlostpw=1')">找回密码</a></td>
+</tr>
+<tr>
+<td><label for="ls_password">密码</label></td>
+<td><input type="password" name="password" id="ls_password" class="px vm" autocomplete="off" tabindex="902" /></td>
+<td class="fastlg_l"><button type="submit" class="pn vm" tabindex="904" style="width: 75px;"><em>登录</em></button></td>
+<td>&nbsp;<a href="member.php?mod=register" class="xi2 xw1">注册</a></td>
+</tr>
+</table>
+<input type="hidden" name="quickforward" value="yes" />
+<input type="hidden" name="handlekey" value="ls" />
+</div>
+
+<div class="fastlg_fm y" style="margin-right: 10px; padding-right: 10px">
+<p><a href="http://www.91xiubbs.com/connect.php?mod=login&op=init&referer=forum.php%3Fmod%3Dattachment%26aid%3DMTA0NTc1fGVmNTY3NmJifDEzNDM4NzU2MzV8NDYwNzI1fDE5ODIyMw%253D%253D%26ck%3Dcb8a1180&statfrom=login_simple"><img src="static/image/common/qq_login.gif" class="vm" alt="QQ登录" /></a></p>
+<p class="hm xg1" style="padding-top: 2px;">只需一步，快速开始</p>
+</div>
+</div>
+</form>
+
+</div>
+
+<div id="nv">
+<a href="javascript:;" id="qmenu" onmouseover="showMenu({'ctrlid':'qmenu','pos':'34!','ctrlclass':'a','duration':2});">快捷导航</a>
+<ul><li id="mn_portal" onmouseover="navShow('portal')"><a href="portal.php" hidefocus="true" title="Portal"  >门户<span>Portal</span></a></li><li class="a" id="mn_forum" onmouseover="navShow('forum')"><a href="forum.php" hidefocus="true" title="BBS"  >论坛<span>BBS</span></a></li><li id="mn_Ne2ff" ><a href="forum-lianmeng-1.html" hidefocus="true" title="LianMeng"  >联盟<span>LianMeng</span></a></li><li id="mn_forum_10" ><a href="forum.php?mod=guide" hidefocus="true" title="Guide"  >导读<span>Guide</span></a></li><li id="mn_home" ><a href="home.php" hidefocus="true" title="Space"  >家园<span>Space</span></a></li><li id="mn_userapp" ><a href="userapp.php" hidefocus="true" title="Manyou"  >游戏<span>Manyou</span></a></li><li id="mn_home_9" ><a href="home.php?mod=follow" hidefocus="true" title="Follow"  >广播<span>Follow</span></a></li><li id="mn_N12a7" ><a href="misc.php?mod=ranklist" hidefocus="true" title="Ranklist"  >排行榜<span>Ranklist</span></a></li><li id="mn_N57b3" ><a href="http://so.91xiu.com" hidefocus="true" title="Search"  >搜索<span>Search</span></a></li><li id="mn_forum_11" ><a href="forum.php?mod=collection" hidefocus="true" title="Collection"  >淘帖<span>Collection</span></a></li><li id="mn_N230d" ><a href="vip.php" hidefocus="true"   style="color: red">VIP</a></li><li id="mn_N462e" ><a href="plugin.php?id=dsu_paulsign:sign" hidefocus="true"   style="color: yellow">每日签到</a></li></ul>
+</div>
+<ul class="p_pop h_pop" id="plugin_menu" style="display: none">  <li><a href="plugin.php?id=dsu_paulsign:sign" id="mn_plink_sign">每日签到</a></li>
+ </ul>
+<div id="mu" class="cl">
+<ul class="cl current" id="snav_mn_forum" style="display:">
+<li><a href="forum-94-1.html" hidefocus="true" >笔记本维修论坛</a></li><li><a href="forum-121-1.html" hidefocus="true" >液晶显示器维修论坛</a></li><li><a href="forum-84-1.html" hidefocus="true" >主板维修论坛</a></li><li><a href="forum-104-1.html" hidefocus="true" >数码相机维修论坛</a></li><li><a href="forum-9-1.html" hidefocus="true" >电脑故障交流</a></li><li><a href="forum-279-1.html" hidefocus="true" >平板电脑维修论坛</a></li></ul>
+</div><div class="a_mu"><DIV class=cl id=xad_mu>
+
+<STYLE type=text/css> 
+		#xad_mu { padding: 15px 0; }
+		#xad_mu div { padding-left: 70px; width: 311px; w\idth: 241px; background-repeat: no-repeat; background-position: 20px 50%; overflow: hidden; }
+		#xad_mu a { float: left; margin-right: 5px; white-space: nowrap; }
+			#xad_mu a:hover { text-decoration: underline; }
+	</STYLE>
+
+<DIV class=z id=xad_mu_l_c style="BORDER-RIGHT: #ddd 1px solid; BACKGROUND-IMAGE: url(/image/91xiuimg/help_32.png)"><A id=xad_mu_l_1_c style="CLEAR: left; FONT-WEIGHT: normal; COLOR: red" href="/forum-284-1.html">晓军哥哥维修技术专区
+</A> <A id=xad_mu_l_2_c style="CLEAR: left; FONT-WEIGHT: normal; COLOR: red" href="#">清口技术-苹果系列维修机构</A> </DIV>
+<DIV class=z id=xad_mu_c_c style="BORDER-RIGHT: #ddd 1px solid; BACKGROUND-IMAGE: url(/image/91xiuimg/recipe_32.png); BORDER-LEFT: #fff 1px solid"><A id=xad_mu_c_1_c style="COLOR: red" href="/home.php?mod=spacecp&ac=credit&op=buy">积分充值金币在线充值</A><A id=xad_mu_c_2_c style="CLEAR: left; COLOR: #999" href="/home.php?mod=spacecp&ac=credit&op=buy">积分充值金币在线充值！</A> </DIV>
+<DIV class=z id=xad_mu_r_c style="BACKGROUND-IMAGE: url(/image/91xiuimg/search_32.png); BORDER-LEFT: #fff 1px solid"><A id=xad_mu_r_1_c href="/home-spacecp-ac-credit-op-rule.html">[导航]积分规则</A><A id=xad_mu_r_2_c style="CLEAR: none; FONT-WEIGHT: normal" href="/home.php?mod=space&do=thread&view=me">[导航]我的帖子</A> <A id=xad_mu_r_3_c style="CLEAR: left; FONT-WEIGHT: normal" href="/home-spacecp-ac-profile-op-password.html">[导航]修改密码</A><A id=xad_mu_r_4_c style="CLEAR: none" href="/home-task-fchannel-xmenuf.html">[导航]任务</A><A id=xad_mu_r_5_c href="/home-spacecp-ac-credit-op-rule.html">[导航]个人资料</A>
+</DIV></DIV></div><div id="scbar" class="scbar_narrow cl">
+<form id="scbar_form" method="post" autocomplete="off" onsubmit="searchFocus($('scbar_txt'))" action="search.php?searchsubmit=yes" target="_blank">
+<input type="hidden" name="mod" id="scbar_mod" value="search" />
+<input type="hidden" name="formhash" value="2458060e" />
+<input type="hidden" name="srchtype" value="title" />
+<input type="hidden" name="srhfid" value="" />
+<input type="hidden" name="srhlocality" value="forum::attachment" />
+<table cellspacing="0" cellpadding="0">
+<tr>
+<td class="scbar_icon_td"></td>
+<td class="scbar_txt_td"><input type="text" name="srchtxt" id="scbar_txt" value="请输入搜索内容" autocomplete="off" x-webkit-speech speech /></td>
+<td class="scbar_type_td"><a href="javascript:;" id="scbar_type" class="showmenu xg1 xs2" onclick="showMenu(this.id)" hidefocus="true">搜索</a></td>
+<td class="scbar_btn_td"><button type="submit" name="searchsubmit" id="scbar_btn" sc="1" class="pn pnc" value="true"><strong class="xi2 xs2">搜索</strong></button></td>
+<td class="scbar_hot_td">
+<div id="scbar_hot">
+<strong class="xw1">热搜: </strong>
+
+<a href="search.php?mod=forum&amp;srchtxt=r400&amp;formhash=2458060e&amp;searchsubmit=true&amp;source=hotsearch" target="_blank" class="xi2" sc="1">r400</a>
+
+
+
+<a href="search.php?mod=forum&amp;srchtxt=R400&amp;formhash=2458060e&amp;searchsubmit=true&amp;source=hotsearch" target="_blank" class="xi2" sc="1">R400</a>
+
+
+
+<a href="search.php?mod=forum&amp;srchtxt=%CA%FD%D7%D6%CA%BE%B2%A8%C6%F7&amp;formhash=2458060e&amp;searchsubmit=true&amp;source=hotsearch" target="_blank" class="xi2" sc="1">数字示波器</a>
+
+
+
+<a href="search.php?mod=forum&amp;srchtxt=%BF%AA%B5%EA&amp;formhash=2458060e&amp;searchsubmit=true&amp;source=hotsearch" target="_blank" class="xi2" sc="1">开店</a>
+
+
+
+<a href="search.php?mod=forum&amp;srchtxt=%B1%CA%BC%C7%B1%BE&amp;formhash=2458060e&amp;searchsubmit=true&amp;source=hotsearch" target="_blank" class="xi2" sc="1">笔记本</a>
+
+
+
+<a href="search.php?mod=forum&amp;srchtxt=x200&amp;formhash=2458060e&amp;searchsubmit=true&amp;source=hotsearch" target="_blank" class="xi2" sc="1">x200</a>
+
+
+
+<a href="search.php?mod=forum&amp;srchtxt=%C9%CF%CD%F8%B1%BE&amp;formhash=2458060e&amp;searchsubmit=true&amp;source=hotsearch" target="_blank" class="xi2" sc="1">上网本</a>
+
+
+
+<a href="search.php?mod=forum&amp;srchtxt=t60&amp;formhash=2458060e&amp;searchsubmit=true&amp;source=hotsearch" target="_blank" class="xi2" sc="1">t60</a>
+
+
+
+<a href="search.php?mod=forum&amp;srchtxt=%BB%AA%CB%B6F8&amp;formhash=2458060e&amp;searchsubmit=true&amp;source=hotsearch" target="_blank" class="xi2" sc="1">华硕F8</a>
+
+
+
+<a href="search.php?mod=forum&amp;srchtxt=led%B0%B5%C6%C1&amp;formhash=2458060e&amp;searchsubmit=true&amp;source=hotsearch" target="_blank" class="xi2" sc="1">led暗屏</a>
+
+
+
+<a href="search.php?mod=forum&amp;srchtxt=sl400&amp;formhash=2458060e&amp;searchsubmit=true&amp;source=hotsearch" target="_blank" class="xi2" sc="1">sl400</a>
+
+
+
+<a href="search.php?mod=forum&amp;srchtxt=%C6%BB%B9%FB%CE%AC%D0%DE&amp;formhash=2458060e&amp;searchsubmit=true&amp;source=hotsearch" target="_blank" class="xi2" sc="1">苹果维修</a>
+
+
+
+<a href="search.php?mod=forum&amp;srchtxt=%BB%AA%CB%B6F5&amp;formhash=2458060e&amp;searchsubmit=true&amp;source=hotsearch" target="_blank" class="xi2" sc="1">华硕F5</a>
+
+
+
+<a href="search.php?mod=forum&amp;srchtxt=X200&amp;formhash=2458060e&amp;searchsubmit=true&amp;source=hotsearch" target="_blank" class="xi2" sc="1">X200</a>
+
+
+
+<a href="search.php?mod=forum&amp;srchtxt=d630&amp;formhash=2458060e&amp;searchsubmit=true&amp;source=hotsearch" target="_blank" class="xi2" sc="1">d630</a>
+
+
+
+<a href="search.php?mod=forum&amp;srchtxt=%D5%D0%C6%B8&amp;formhash=2458060e&amp;searchsubmit=true&amp;source=hotsearch" target="_blank" class="xi2" sc="1">招聘</a>
+
+
+
+<a href="search.php?mod=forum&amp;srchtxt=sl410k&amp;formhash=2458060e&amp;searchsubmit=true&amp;source=hotsearch" target="_blank" class="xi2" sc="1">sl410k</a>
+
+
+
+<a href="search.php?mod=forum&amp;srchtxt=y450&amp;formhash=2458060e&amp;searchsubmit=true&amp;source=hotsearch" target="_blank" class="xi2" sc="1">y450</a>
+
+
+
+<a href="search.php?mod=forum&amp;srchtxt=E40&amp;formhash=2458060e&amp;searchsubmit=true&amp;source=hotsearch" target="_blank" class="xi2" sc="1">E40</a>
+
+
+
+<a href="search.php?mod=forum&amp;srchtxt=cq40&amp;formhash=2458060e&amp;searchsubmit=true&amp;source=hotsearch" target="_blank" class="xi2" sc="1">cq40</a>
+
+</div>
+</td>
+</tr>
+</table>
+</form>
+</div>
+<ul id="scbar_type_menu" class="p_pop" style="display: none;"><li><a href="javascript:;" rel="article">文章</a></li><li><a href="javascript:;" rel="forum" class="curtype">帖子</a></li><li><a href="javascript:;" rel="blog">日志</a></li><li><a href="javascript:;" rel="album">相册</a></li><li><a href="javascript:;" rel="group">群组</a></li><li><a href="javascript:;" rel="user">用户</a></li></ul>
+<script type="text/javascript">
+initSearchmenu('scbar', '');
+</script>
+</div>
+</div>
+
+
+<div id="wp" class="wp"><div id="ct" class="wp cl w">
+<div class="nfl" id="main_succeed" style="display: none">
+<div class="f_c altw">
+<div class="alert_right">
+<p id="succeedmessage"></p>
+<p id="succeedlocation" class="alert_btnleft"></p>
+<p class="alert_btnleft"><a id="succeedmessage_href">如果您的浏览器没有自动跳转，请点击此链接</a></p>
+</div>
+</div>
+</div>
+<div class="nfl" id="main_message">
+<div class="f_c altw">
+<div id="messagetext" class="alert_info">
+<p>抱歉，只有特定用户可以下载本站附件</p>
+</div>
+<div id="messagelogin"></div>
+<script type="text/javascript">ajaxget('member.php?mod=logging&action=login&infloat=yes&frommessage', 'messagelogin');</script>
+</div>
+</div>
+</div>	</div>
+<div class="wp a_f"><!--// 帖子防复制自动加版权代码开始-->
+
+
+
+<script type="text/javascript">
+
+
+
+document.body.oncopy = function () { 
+
+
+
+setTimeout( function () { 
+
+
+
+  var text = clipboardData.getData("text");
+
+
+
+  if (text) { 
+
+
+
+   text = text + "\r\n本文来自: 91修中华维修网(http://www.91xiubbs.com) 详细文章参考："+location.href; 
+
+
+
+clipboardData.setData("text", text);
+
+
+
+  } 
+
+
+
+    }, 100 ) 
+
+
+
+}
+
+
+
+</script>
+
+
+
+<!--// 帖子防复制自动加版权代码结束-->
+</div>
+
+<script type="text/javascript">
+_attachEvent(window, 'load', getForbiddenFormula, document);
+function getForbiddenFormula() {
+var toGetForbiddenFormulaFIds = function () {
+ajaxget('plugin.php?id=cloudsearch&formhash=2458060e');
+};
+var a = document.body.getElementsByTagName('a');
+for(var i = 0;i < a.length;i++){
+if(a[i].getAttribute('sc')) {
+a[i].setAttribute('mid', hash(a[i].href));
+a[i].onmousedown = function() {toGetForbiddenFormulaFIds();};
+}
+}
+var btn = document.body.getElementsByTagName('button');
+for(var i = 0;i < btn.length;i++){
+if(btn[i].getAttribute('sc')) {
+btn[i].setAttribute('mid', hash(btn[i].id));
+btn[i].onmousedown = function() {toGetForbiddenFormulaFIds();};
+}
+}
+}
+</script>
+
+<div id="ft" class="wp cl">
+<div id="flk" class="y">
+<p><a href="forum.php?mobile=yes" >手机版</a><span class="pipe">|</span><a href="#" >广告联系QQ:9459803</a><span class="pipe">|</span><a href="http://www.91xiubbs.com/forum.php?mod=rss&auth=0" >RSS订阅</a><span class="pipe">|</span><strong><a href="http://www.91xiu.com/" target="_blank">91修网</a></strong>
+&nbsp;<a href="http://discuz.qq.com/service/security" target="_blank" title="防水墙保卫网站远离侵害"><img src="static/image/common/security.png"></a>&nbsp;&nbsp;<span id="tcss"></span><script type="text/javascript" src="http://tcss.qq.com/ping.js?v=1VERHASH" charset="utf-8"></script><script type="text/javascript" reload="1">pgvMain({"discuzParams":{"r2":"1053824","ui":0,"rt":"forum","md":"attachment","pn":1,"qq":"000"},"extraParams":""});</script><script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fd8af90655b20ecd682cd8536ae27cdb9' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script src='http://s3.cnzz.com/stat.php?id=1645&web_id=8422' language='JavaScript' charset='gb2312'></script></p>
+<p class="xs0">
+GMT+8, 2012-8-2 10:48<span id="debuginfo">
+, Processed in 0.048478 second(s), 3 queries
+, Memcache On.
+</span>
+</p>
+</div>
+<div id="frt">
+<p>Powered by <strong><a href="http://www.discuz.net" target="_blank">Discuz!</a></strong> <em>X2.5</em></p>
+<p class="xs0">&copy; 2001-2012 <a href="http://www.comsenz.com" target="_blank">Comsenz Inc.</a></p>
+</div></div>
+
+<div id="g_upmine_menu" class="tip tip_3" style="display:none;">
+<div class="tip_c">
+积分 0, 距离下一级还需  积分
+</div>
+<div class="tip_horn"></div>
+</div>
+<script src="home.php?mod=misc&ac=sendmail&rand=1343875700" type="text/javascript"></script>
+<span id="scrolltop" onclick="window.scrollTo('0','0')">回顶部</span>
+<script type="text/javascript">_attachEvent(window, 'scroll', function(){showTopLink();});</script>
+			<div id="discuz_tips" style="display:none;"></div>
+			<script type="text/javascript">
+				var discuzSId = '1053824';
+				var discuzVersion = 'X2.5';
+				var discuzRelease = '20120701';
+				var discuzApi = '0.6';
+				var discuzIsFounder = '';
+				var discuzFixbug = '25000002';
+				var discuzAdminId = '0';
+				var discuzOpenId = '';
+				var discuzUid = '0';
+				var discuzGroupId = '7';
+				var ts = '1343875700';
+				var sig = 'aef8cc3022c662d3cc7d848a0bdd32f3';
+				var discuzTipsCVersion = '2';
+			</script>
+			<script src="http://discuz.gtimg.cn/cloud/scripts/discuz_tips.js?v=1" type="text/javascript" charset="UTF-8"></script></body>
+</html>
